4de0e6ef12acd252c25abf081b280586ac07eab5
Abdullah34765 <abdullahkhang808@gmail.com>
2025-08-20 18:27:42 +0500

    Ignore ESLint errors during build for Vercel

--- todo/next.config.ts
+++ todo/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ignore ESLint errors during Vercel build
+  },
 };
 
 export default nextConfig;
